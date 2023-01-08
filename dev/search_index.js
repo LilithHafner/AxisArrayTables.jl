197e17b6f4ece82cd109e73238a9b6c67f4f8e14
Documenter.jl <documenter@juliadocs.github.io>
2023-01-08 19:38:29 +0000

    build based on 11aeba1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AxisArrayTables","category":"page"},{"location":"#AxisArrayTables","page":"Home","title":"AxisArrayTables","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AxisArrayTables.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AxisArrayTables]","category":"page"}]
+}
